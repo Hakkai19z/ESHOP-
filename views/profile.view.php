e6b01df360ced9d32df415f5e96b72e613d5fcff
Hakkai19z <kahil.mokhtari@gmail.com>
2024-02-01 16:20:34 +0100

    contact-page

--- views/profile.view.php
+++ views/profile.view.php
@@ -6,8 +6,10 @@ session_start();
 ?>
 
 <div class="wrapper">
-    <h1>Profile de <?= $_SESSION['user']['name'] ?></h1>
+    <h1>Profil de <?= $_SESSION['user']['name'] ?></h1>
     <h2>Votre adresse mail : <?= $_SESSION['user']['email'] ?></h2>
+
+    <img id='avatar' class='profile-avatar' src="../uploads/avatar/myh.jpg" alt="">
 </div>
 
 <?php include '../partials/footer.php' ?>
